981e2d24c415833ab584765d0da30e4b2506930f
Danny Kirschner <rkirschner1377@gmail.com>
2017-01-08 16:54:51 -0500

    adding hello express

--- hello-express/index.js
+++ hello-express/index.js
@@ -5,3 +5,10 @@ app.listen(4000, () => {
   console.log("app listening on port 4000");
 })
 
+app.get("/", (req, res) => {
+  res.send("Hello, world!");
+});
+
+app.get("/:name", (req, res) => {
+  res.send(`hello ${req.params.name}`);
+});
